193923c1efd410a0d1545cf4c9517d9badc9d413
realJimboSlice <jimm7493@stud.kea.dk>
2024-03-20 15:17:19 +0100

    Updated location function - starting on the buy functions

--- script.js
+++ script.js
@@ -37,6 +37,12 @@ const locations = [
     "button functions": [buyHealth, buyWeapon, goTown],
     text: "You enter the store.",
   },
+  {
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You enter the cave. Ýou see some monsters.",
+  },
 ];
 
 //Initialize buttons
@@ -47,36 +53,37 @@ button3.onclick = fightDragon;
 // Functions
 
 function goTown() {
-  button1.innerText = "Go to store";
-  button2.innerText = "Go to cave";
-  button3.innerText = "Fight dragon";
-  text.innerText =
-    'You are in the town square. You see a sign that says "Store".';
-  button1.onclick = goStore;
-  button2.onclick = goCave;
-  button3.onclick = fightDragon;
+  update(locations[0]);
 }
 
 function goStore() {
-  button1.innerText = "Buy 10 health (10 gold)";
-  button2.innerText = "Buy weapon (30 gold)";
-  button3.innerText = "Go to town square";
-  text.innerText = "You enter the store.";
-  button1.onclick = buyHealth;
-  button2.onclick = buyWeapon;
-  button3.onclick = goTown;
+  update(locations[1]);
 }
 
 function goCave() {
-  console.log("Going to cave.");
+  update(locations[2]);
 }
 
 function fightDragon() {
   console.log("Fighting dragon.");
 }
 
-function buyHealth() {}
+function buyHealth() {
+  gold = gold - 10;
+}
 
 function buyWeapon() {}
 
-function update(location) {}
+function update(location) {
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  text.innerText = location.text;
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+}
+
+function fightSlime() {}
+
+function fightBeast() {}

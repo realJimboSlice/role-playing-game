c8d8bc7e38638f280a986df82faecb6bf8edd502
realJimboSlice <jimm7493@stud.kea.dk>
2024-03-20 14:17:24 +0100

    Added monster spans in html and added text intro

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ let fighting;
 let monsterHealth;
 let inventory = ["stick"];
 
-let button1 = document.querySelector("#button1");
-let button2 = document.querySelector("#button2");
-let button3 = document.querySelector("#button3");
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");

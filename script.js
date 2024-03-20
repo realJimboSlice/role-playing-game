a8ce0b1acb762ae8a8a57e66caeb5c64590ce425
realJimboSlice <jimm7493@stud.kea.dk>
2024-03-20 14:13:51 +0100

    added qselectors and movd script tag in html

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
+
+let button1 = document.querySelector("#button1");
+let button2 = document.querySelector("#button2");
+let button3 = document.querySelector("#button3");

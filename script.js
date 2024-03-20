2554e7ad57f0724b040e9108ad459fbbd6fffe0c
realJimboSlice <jimm7493@stud.kea.dk>
2024-03-20 14:30:19 +0100

    Styling and added id variables

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Variables
 let xp = 0;
 let health = 100;
 let gold = 50;
@@ -6,6 +7,16 @@ let fighting;
 let monsterHealth;
 let inventory = ["stick"];
 
+// Button variables
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+
+// Id variables
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
